1ca457f3ed1a8284e12216b4319095be2ab3dde8
kaarf <rpdirwk1025@naver.com>
2026-09-13 19:34:32 +0900

    [test] #97 - 분산 락 직렬화 및 안전한 해제 검증

---
+++ backend/src/test/java/junsik/reservation/support/MySqlRedisIntegrationTestSupport.java
@@ -0,0 +1,23 @@
+package junsik.reservation.support;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public abstract class MySqlRedisIntegrationTestSupport extends MySqlIntegrationTestSupport {
+
+	protected static final GenericContainer<?> REDIS;
+
+	static {
+		REDIS = new GenericContainer<>(DockerImageName.parse("redis:7.4"))
+				.withExposedPorts(6379);
+		REDIS.start();
+	}
+
+	@DynamicPropertySource
+	static void configureRedis(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", REDIS::getHost);
+		registry.add("spring.data.redis.port", () -> REDIS.getMappedPort(6379));
+	}
+}
